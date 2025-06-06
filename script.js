5626f679ef85e5dc0f2a0dfc1e6830cb93b53ed6
Dhairya <sharmajikaemail123@gmail.com>
2025-06-06 23:38:09 +0530

    Added flip on click

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ dark.addEventListener("click", function(){
         count = 0;
 }
 });
+
+
+const cards = document.querySelectorAll(".memory-card");
+function flipcard(){
+    this.classList.toggle("flip");
+}
+cards.forEach(card => card.addEventListener("click", flipcard))
