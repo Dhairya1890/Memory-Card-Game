6d04fcf70e6d6f6b8a511f01046e13549a60ee6e
Dhairya <sharmajikaemail123@gmail.com>
2025-06-09 23:17:49 +0530

    completed game 1

--- script.js
+++ script.js
@@ -1,21 +1,181 @@
-const dark = document.getElementById("dark");
-const body = document.body;
-let count = 0;
-dark.addEventListener("click", function(){
-    if(count == 0){
-        body.classList.add("darkactive");
-    count = 1;
-    }
-    
-    else{
-        body.classList.remove("darkactive");
-        count = 0;
-}
-});
 
-
-const cards = document.querySelectorAll(".memory-card");
+const card1 = document.querySelectorAll(".memory-card");
 function flipcard(){
     this.classList.toggle("flip");
 }
-cards.forEach(card => card.addEventListener("click", flipcard))
+card1.forEach(card => card.addEventListener("click", flipcard))
+
+
+//   const cards = document.querySelectorAll('.memory-card');
+//   let hasFlippedCard = false;
+//   let lockBoard = false;
+//   let firstCard, secondCard;
+//   let score = 0;
+
+//   const scoreDisplay = document.getElementById('score');
+
+//   function flipCard() {
+//     if (lockBoard || this === firstCard) return;
+
+//     this.classList.add('flip');
+
+//     if (!hasFlippedCard) {
+//       hasFlippedCard = true;
+//       firstCard = this;
+//       return;
+//     }
+
+//     secondCard = this;
+//     checkForMatch();
+//   }
+
+//   function checkForMatch() {
+//     const firstImage = firstCard.querySelector('.back-face img').src;
+//     const secondImage = secondCard.querySelector('.back-face img').src;
+
+//     if (firstImage === secondImage) {
+//       // It's a match!
+//       score += 1;
+//       updateScore();
+//       disableCards();
+//     } else {
+//       unflipCards();
+//     }
+//   }
+
+//   function updateScore() {
+//     scoreDisplay.textContent = score;
+//   }
+
+//   function disableCards() {
+//     firstCard.removeEventListener('click', flipCard);
+//     secondCard.removeEventListener('click', flipCard);
+//     resetBoard();
+//   }
+
+//   function unflipCards() {
+//     lockBoard = true;
+//     setTimeout(() => {
+//       firstCard.classList.remove('flip');
+//       secondCard.classList.remove('flip');
+//       resetBoard();
+//     }, 1000);
+//   }
+
+//   function resetBoard() {
+//     [hasFlippedCard, lockBoard] = [false, false];
+//     [firstCard, secondCard] = [null, null];
+//   }
+
+//   // Attach event listener to all cards
+//   cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+
+  const images = [
+    'assests/cyclone.png',
+    'assests/heart.png',
+    'assests/large_blue_circle.png',
+    'assests/large_blue_diamond.png',
+    'assests/small_red_triangle.png',
+    'assests/star.png',
+    'assests/star2.png',
+    'assests/white_large_square.png'
+  ];
+
+  const board = document.getElementById('game-board');
+  const scoreDisplay = document.getElementById('score');
+  let firstCard, secondCard;
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let score = 0;
+
+  function createCards() {
+    const cardImages = [...images, ...images]; // Duplicate for pairs
+    shuffle(cardImages);
+
+    board.innerHTML = ''; // Clear previous cards
+
+    cardImages.forEach(src => {
+      const card = document.createElement('div');
+      card.classList.add('memory-card');
+      card.setAttribute('data-image', src);
+
+      card.innerHTML = `
+        <div class="front-face"><img src="assests/Neuron Games.png" alt="back"></div>
+        <div class="back-face"><img src="${src}" alt="front"></div>`;
+
+      card.addEventListener('click', flipCard);
+      board.appendChild(card);
+    });
+  }
+
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function flipCard() {
+    if (lockBoard || this === firstCard || this.classList.contains('flip')) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
+    }
+
+    secondCard = this;
+    lockBoard = true;
+
+    checkForMatch();
+  }
+
+  function checkForMatch() {
+    const isMatch = firstCard.dataset.image === secondCard.dataset.image;
+
+    if (isMatch) {
+      score += 1;
+      updateScore();
+      disableCards();
+    } else {
+      unflipCards();
+    }
+  }
+
+  function updateScore() {
+    scoreDisplay.textContent = score;
+  }
+
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+  }
+
+  function unflipCards() {
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      resetBoard();
+    }, 1000);
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+  function startGame() {
+    score = 0;
+    updateScore();
+    createCards();
+  }
+
+  window.onload = startGame;
+
+
